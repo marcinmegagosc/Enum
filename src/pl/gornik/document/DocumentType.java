909c3283edc9a3e9c27dfc55c7aee0eb72afbd39
marcinmegagosc <marcin.sss221@gmail.com>
2024-01-12 08:54:38 +0100

    zadanie zrobione

--- src/pl/gornik/document/DocumentType.java
+++ src/pl/gornik/document/DocumentType.java
@@ -1,14 +1,16 @@
 package pl.gornik.document;
 
 public enum DocumentType {
-    INVOICE("faktura"),
-    CONTRACT("kontrakt"),
-    NOTARIAL_ACT("akt notarialny"),
-    CERTYFICATE("certyfikat");
+    INVOICE("faktura",1),
+    CONTRACT("kontrakt",2),
+    NOTARIAL_ACT("akt notarialny",3),
+    CERTYFICATE("certyfikat",4);
 
     private String document;
+    private int id;
 
-    DocumentType(String document) {
+    DocumentType(String document, int id) {
         this.document = document;
+        this.id = id;
     }
 }
--- src/pl/gornik/document/Main.java
+++ src/pl/gornik/document/Main.java
@@ -11,7 +11,7 @@ public class Main {
         List<Document> documentList = new ArrayList<>();
         documentList.add(new Document("Faktura na żelazko",1,DocumentType.INVOICE));
         documentList.add(new Document("Kontrakt zawodowy",3,DocumentType.CONTRACT));
-        documentList.add(new Document("Akt notarialny",5,DocumentType.NOTARIAL_ACT));
+        documentList.add(new Document("Akt zakupienia domu",5,DocumentType.NOTARIAL_ACT));
         documentList.add(new Document("Certyfikat ukończenia szkolenia",1,DocumentType.CERTYFICATE));
 
         for(Document Document: documentList) System.out.println(Document);
@@ -21,6 +21,63 @@ public class Main {
         String name = scanner.nextLine();
         System.out.println("Wprowadź ilość stron dokumentu");
         int pageAmount = scanner.nextInt();
-        System.out.println("Podaj typ dokumentu ");
+        scanner.nextLine();
+        System.out.println("Podaj typ dokumentu. Podaj nazwe, lub numer id: 1. faktura, 2. kontrakt, 3. akt notarialny, 4. certyfikat");
+        System.out.println("Wprowadź nazwę");
+        String documentName = scanner.nextLine();
+        DocumentType docType = getDocument(documentName);
+        System.out.println("Wprowadź ID");
+        int documentId = scanner.nextInt();
+        DocumentType docID = getDocument(documentId);
+        if(docType != null){
+            documentList.add(new Document(name,pageAmount,docType));
+            System.out.println("Utworzono obiekt dokumentu poprzez nazwę");
+        }else System.out.println("Wprowadziłeś wartość z poza ENUM");
+        if(docID != null){
+            documentList.add(new Document(name,pageAmount,docID));
+            System.out.println("Utworzono obiekt dokumentu poprzez ID");
+        }else System.out.println("Wprowadziłeś wartość z poza ENUM");
+        for(Document Document: documentList) System.out.println(Document);
+
     }
+
+    public static DocumentType getDocument(String documentName){
+        switch(documentName){
+            case("faktura") ->{
+                return DocumentType.INVOICE;
+            }
+            case("kontrakt") ->{
+                return DocumentType.CONTRACT;
+            }
+            case("akt notarialny")->{
+                return DocumentType.NOTARIAL_ACT;
+            }
+            case("certyfikat")->{
+                return DocumentType.CERTYFICATE;
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+    public static DocumentType getDocument(int documentID){
+        switch(documentID){
+            case(1) ->{
+                return DocumentType.INVOICE;
+            }
+            case(2) ->{
+                return DocumentType.CONTRACT;
+            }
+            case(3)->{
+                return DocumentType.NOTARIAL_ACT;
+            }
+            case(4)->{
+                return DocumentType.CERTYFICATE;
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
 }
